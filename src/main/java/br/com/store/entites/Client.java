1bf91b65241a215f663106eee77c2fe15c3a80e2
Vinicius Jesus <92672912+viniciusantos2105@users.noreply.github.com>
2022-08-31 14:53:32 -0300

    test

--- src/main/java/br/com/store/entites/Client.java
+++ src/main/java/br/com/store/entites/Client.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @Getter
 @Setter
+@Data
 @Entity
 @Builder
 @NoArgsConstructor
@@ -38,7 +39,8 @@ public class Client implements Serializable {
     @JoinColumn(name = "tb_address")
     private Address address;
 
-    @OneToMany(cascade=CascadeType.PERSIST, mappedBy = "client")
+    @OneToMany(cascade=CascadeType.PERSIST)
+    @JoinColumn(name = "tb_purchaseRecord")
     @JsonManagedReference
     private List<Request> purchaseRecord = new ArrayList<>();
 
